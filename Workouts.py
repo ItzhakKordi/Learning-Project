5811d3db812a73df02e7f3005d06957036ae36f4
ItzhakKordi <54023686+ItzhakKordi@users.noreply.github.com>
2019-08-12 13:32:14 +0300

    Add files via upload

---
+++ Workouts.py
@@ -0,0 +1,38 @@
+#workout cal
+listofpeople = []
+person1 = []
+listofpeople.append(person1)
+numberofpeople = int(input("enter the number of people "))
+def personinfo(person):
+	name =input("whats your name? ")
+	person.append(name)
+	haight = float(input("whats your haight"))
+	person.append(haight)
+	waight = float(input("whats your waight"))
+	person.append(waight)
+	print(person)
+def peoplecreation(listofpeople):
+	for p in range(numberofpeople):
+		person = []
+		personinfo(person)
+		listofpeople.append(person)
+
+personinfo(person1)
+listofpeople.append(person1)
+#1 person is in
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+input()
+
